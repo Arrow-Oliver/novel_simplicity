1f2444bc931eab752165fe5f245030c07f2404ab
Arrow <1619958177@qq.com>
2022-09-27 09:14:00 +0800

    test: 测试分支

---
+++ src/test/java/com/zua/novel/Test01.java
@@ -0,0 +1,15 @@
+package com.zua.novel;
+
+import com.zua.core.common.util.ImgVerifyCodeUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Arrow
+ * @date 2022/8/12 9:35
+ */
+@SpringBootTest
+public class Test01 {
+
+
+}
